053fb643bc5a72701afbe8893e7e45021b962669
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-08-13 18:35:07 +0530

    Map is now working for the current location

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoiZ29sZHlraW5nMTkwNyIsImEiOiJja3J0OGE4b3MwenUyMm5xY3ZodTdxNWV6In0.XE1Rqpr4LYSp4zEJLXaf2w";
 
-var map = new mapboxgl.Map({
-  accessToken: MAPBOX_ACCESS_TOKEN,
-  container: "map",
-  style: "mapbox://styles/mapbox/streets-v11",
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
 });
+
+function setupMap(centerPosition) {
+  const map = new mapboxgl.Map({
+    accessToken: MAPBOX_ACCESS_TOKEN,
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: centerPosition,
+    zoom: 10,
+  });
+}
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude]);
+}
+function errorLocation() {
+  setupMap([-2.24, 53.48]);
+}
